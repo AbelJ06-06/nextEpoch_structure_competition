5e43a7da95655921876111a71f0da359e3f99b86
Abel John Koshy <142508727+AbelJ06-06@users.noreply.github.com>
2024-08-09 04:40:52 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from src.data import get_dataloaders
 from src.model import RNA_net
 from src.util import compute_f1, compute_precision, compute_recall, plot_structures
 from src.submission_formatter import format_submission
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 import torch
 import torch.nn as nn
@@ -13,9 +14,8 @@ train_loader, val_loader, test_loader = get_dataloaders(batch_size = 8, max_leng
 # Init model, loss function, optimizer
 model = RNA_net(embedding_dim=64).to(device)
 criterion = nn.BCEWithLogitsLoss(pos_weight=torch.tensor([300])).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001, momentum=0.9)
-scheduler1 = ExponentialLR(optimizer, gamma=0.9)
-
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=3, verbose=True)
 # Training loop
 train_losses = []
 valid_losses = []
@@ -44,8 +44,6 @@ for epoch in range(20):
         loss_train += loss.item()
         f1_train += compute_f1(y_pred, y)
 
-    scheduler1 = ExponentialLR(optimizer, gamma=0.9)
-
     for batch in val_loader:
         x = batch["sequence"].to(device) # (N, L)
         y = batch['structure'].to(device) # (N, L, L)
@@ -62,6 +60,8 @@ for epoch in range(20):
 
     f1s_train.append(f1_train/len(train_loader))
     f1s_valid.append(f1_valid/len(val_loader))
+
+    scheduler.step(loss_valid)
 
     print(f"Epoch {epoch}, F1 train: {f1s_train[-1]:.2f}, F1 valid: {f1s_valid[-1]:.2f}")
 
